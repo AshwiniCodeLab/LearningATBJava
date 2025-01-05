83aec9d65f506ed7fa52a3019e29bbb54b690a2e
Ashwini <suryavanshi264@gmail.com>
2025-01-05 13:08:52 +0530

    Task5

---
+++ src/JavaBasics/Lab003_Comments.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Lab003_Comments {
+    public static void main(String[] args) {
+        // this is single line comment
+        System.out.println("This // is single line comment ");
+        /*this is multiline comment*/
+        System.out.println("This /**/ is multiline command");
+    }
+}

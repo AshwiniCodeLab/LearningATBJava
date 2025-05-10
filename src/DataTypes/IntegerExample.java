bae35ae79886ea8e4dff5da85b29d5d4fc8fdc66
Ashwini <suryavanshi264@gmail.com>
2025-05-10 22:26:13 +0530

    Day1/10thMay

---
+++ src/DataTypes/IntegerExample.java
@@ -0,0 +1,8 @@
+package DataTypes;
+
+public class IntegerExample {
+    public static void main(String[] args){
+        int number=10;
+        System.out.println("Integer is: "+number);
+    }
+}

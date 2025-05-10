745fcd66e297e7306665a1c9d0b7366a5f1d9b49
Ashwini <suryavanshi264@gmail.com>
2025-05-11 00:08:21 +0530

    Day1/10thMay

---
+++ src/JavaBasics/Lab007_Printf.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class Lab007_Printf {
+    public static void main(String[] args) {
+        int num=9;
+        for(int i=0;i<11;i++){
+            System.out.printf("%d * %d= %d%n ",num,i,num*i );
+        }
+    }
+}
+
+

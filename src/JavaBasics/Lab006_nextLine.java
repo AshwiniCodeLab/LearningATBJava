296729fdef334dabe767569a8da036a84aacabba
Ashwini <suryavanshi264@gmail.com>
2025-05-10 22:25:24 +0530

    Day1/10thMay

---
+++ src/JavaBasics/Lab006_nextLine.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Lab006_nextLine {
+public static void main(String[] args){
+
+    System.out.println("I am from method println");
+    System.out.print("I am from print method");
+}
+
+}

25015f8e6dbc8954ab79c4be23b879fd5187ea9c
Ashwini <suryavanshi264@gmail.com>
2025-06-23 09:18:14 +0530

    task

---
+++ src/JavaBasics/DateDemo.java
@@ -0,0 +1,9 @@
+package JavaBasics;
+import java.util.*;
+public class DateDemo {
+    public static void main(String[] args) {
+        //to display current date and time
+        Date cd= new Date();
+        System.out.println(cd.toString());
+    }
+}
